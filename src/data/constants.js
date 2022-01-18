5ef63b19d8634310e6921f1c968016c716ece057
Yoon <minyoon6725@gmail.com>
2022-01-18 20:37:16 +0800

    changing of code

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,12 @@
 export const projects = [
+    
     {
-      title: 'Simple CRM MERN Web App',
-      description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-        image: '/Calling_Outline.svg',
-        tags: ['Mongo', 'Express', 'React', 'Node'],
-      source: 'https://google.com',
-      visit: 'https://google.com',
-      id: 0,
-    },
-    {
-      title: 'To-Do-List',
-      description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+      title: 'Simple CRUD Web Api',
+      description:"This is a Web APi build up by using Node Js and Express, which get the data from MSSQL database",
       image: '/Note taking_Isometric.svg',
-      tags: ['React', 'JavaScript'],
-      source: 'https://google.com',
-      visit: 'https://google.com',
+      tags: ['Node Js', 'Express','MSSQL'],
+      source: 'https://github.com/Yoon9811/simple-crud-web-api/tree/master',
+      visit: '#',
       id: 1
     }
   ];
